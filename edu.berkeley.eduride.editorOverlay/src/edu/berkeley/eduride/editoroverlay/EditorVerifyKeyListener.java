6ab206ed1ed9c9f94d6bb5c600e28a1277d5e999
kimberlyjayne <kimberlyjayne@berkeley.edu>
2013-10-24 18:10:12 -0700

    Multiline boxes now drawn from markers, still buggy on expand/collapse

--- edu.berkeley.eduride.editorOverlay/src/edu/berkeley/eduride/editoroverlay/EditorVerifyKeyListener.java
+++ edu.berkeley.eduride.editorOverlay/src/edu/berkeley/eduride/editoroverlay/EditorVerifyKeyListener.java
@@ -1,6 +1,7 @@
 package edu.berkeley.eduride.editoroverlay;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.ITextSelection;
@@ -39,6 +40,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.List;
 
 public class EditorVerifyKeyListener implements VerifyKeyListener {
 
@@ -53,6 +55,7 @@ public class EditorVerifyKeyListener implements VerifyKeyListener {
 	
 	private StyledText boxText;
 	private BoxPaintListener boxPaint;
+	IResource res;
 	
 	public EditorVerifyKeyListener(IEditorPart editor) {
 		this.installMe(editor);
@@ -69,18 +72,18 @@ public class EditorVerifyKeyListener implements VerifyKeyListener {
 				installedOn.put(editor, ekpl);
 				
 				//TODO:  Remove this, testing only!
-				ekpl.boxList.add(new MultilineBox(2, 5));
-				ekpl.boxList.add(new MultilineBox(7, 15, new Color(null, 255,100,100)));
+				//ekpl.boxList.add(new MultilineBox(2, 5));
+				//ekpl.boxList.add(new MultilineBox(7, 15, new Color(null, 255,100,100)));
 				//ekpl.boxList.add(new MultilineBox(17, 17, new Color(null, 255,255,100)));
 				//ekpl.boxList.add(new MultilineBox(19, 24, new Color(null, 255,100,255)));
 				//ekpl.boxList.add(new MultilineBox(25, 27, new Color(null, 100,255,160)));
 				
-				IResource res = ResourceUtil.getResource(editor.getEditorInput());
-				if (res != null) {	
+				ekpl.res = ResourceUtil.getResource(editor.getEditorInput());
+				if (ekpl.res != null) {	
 					System.out.println("making inline");
-					AllowEditing.createInLine(res, 10, 5, 25, "yeah");
+					AllowEditing.createInLine(ekpl.res, 10, 5, 25, "yeah");
 					System.out.println("making multiline");
-					AllowEditing.createMultiLine(res, 12, 16, "yeah2");
+					AllowEditing.createMultiLine(ekpl.res, 12, 16, "yeah2");
 				}
 			}
 			ekpl.toggle();  //TODO: Move this to a logical place...  it toggles behavior on/off
@@ -126,7 +129,7 @@ public class EditorVerifyKeyListener implements VerifyKeyListener {
 			this.sel = (ITextSelection) sp.getSelection();
 			sp.addSelectionChangedListener(new selChanged(this));
 		} else {
-			System.out.println("loggerInstall " + "EditorKeyPressListener failed to installed in " + editor.getTitle());
+			System.out.println("loggerInstall " + "EditorVerifyKeyListener failed to installed in " + editor.getTitle());
 		}
 
 	}
@@ -162,15 +165,9 @@ public class EditorVerifyKeyListener implements VerifyKeyListener {
 		drawBox();
 	}
 	
-	//TODO: Figure out if we need these listeners or not  (looks like it's working with just paintlistener)
-	//private BoxKeyListener boxKey;
-	//private BoxModifyListener boxModify;
-	//private BoxMouseMoveListener boxMouseMove;
-	//private BoxMouseTrackListener boxMouseTrack;
-	//private BoxTextChangeListener boxTextChange;
-	
 	private void toggle() {
 		if (!turnedOn) {
+			createBoxListFromMarkers();
 			decorate();
 		} else {
 			undecorate();
@@ -179,7 +176,7 @@ public class EditorVerifyKeyListener implements VerifyKeyListener {
 		drawBox();
 	}
 	
-	//install various listeners for drawing
+	//install any listeners for drawing
 	private void decorate() {
 		if (turnedOn == true) {  //already on!
 			return;
@@ -191,33 +188,12 @@ public class EditorVerifyKeyListener implements VerifyKeyListener {
         boxText.addPaintListener(boxPaint);
 	}
 	
-	//stop random listening when turned off
+	//stop listening when turned off
 	private void undecorate() {
 		turnedOn = false;
 		//Stop listeners
 		boxText.removePaintListener(boxPaint);
 	}
-	
-//	private class BoxKeyListener implements KeyListener {
-//		public void keyPressed(KeyEvent e) { }
-//		
-//		public void keyReleased(KeyEvent e) {
-//			drawBox();
-//		}
-//	}
-//	
-//	private class BoxModifyListener implements ModifyListener {		//Can we use modifylistener to fix ctrl+v?
-//		//When text actually changes
-//		public void modifyText(ModifyEvent e) {
-//			drawBox();
-//		}
-//	}
-//	
-//	private class BoxMouseMoveListener implements MouseMoveListener {
-//		public void mouseMove(MouseEvent e) {
-//			drawBox();
-//		}
-//	}
 	
 	private class BoxPaintListener implements PaintListener {
 		@Override
@@ -227,13 +203,29 @@ public class EditorVerifyKeyListener implements VerifyKeyListener {
 	}
 	
 	
+	//Find all markers on res, store them as pairs in boxes in boxList
+	private void createBoxListFromMarkers() {
+		List<IMarker[]> multiline = AllowEditing.getMultilineMarkers(res);   //TODO:  Check this with AllowEditing's methods
+		MultilineBox b;
+		int start, stop;
+		for (int i = 0; i < multiline.size(); i++) {
+			b = new MultilineBox(multiline.get(i)[0], multiline.get(i)[1]);
+			boxList.add(b);
+		}
+		
+		List<IMarker> inline = AllowEditing.getInlineMarkers(res);
+		//TODO: Storage and drawing for inline markers
+	}
+	
+	
+	
 	
 	//ATTEMPT #1 AT BOX DRAWING
 	//Most of this is stolen from editbox
 	
 	private LinkedList<Integer> oldDrawParameters;  //Stores some set of numbers related to things we're drawing, not for use outside Draw!
 	
-	//start and stop are line numbers to draw the box around, should probably just store them as instance variables...
+	//draws boxes from boxList
 	public void drawBox() {
 		
 		//big picture: create an image (newImage), edit with the gc, then set as styledtext background later
--- edu.berkeley.eduride.editorOverlay/src/edu/berkeley/eduride/editoroverlay/MultilineBox.java
+++ edu.berkeley.eduride.editorOverlay/src/edu/berkeley/eduride/editoroverlay/MultilineBox.java
@@ -1,30 +1,40 @@
 package edu.berkeley.eduride.editoroverlay;
 
+import org.eclipse.core.resources.IMarker;
 import org.eclipse.swt.graphics.Color;
+
+import edu.berkeley.eduride.editoroverlay.marker.AllowEditing;
 
 public class MultilineBox {
 	//start and stop are the line numbers corresponding to where user can type
-	private int start;
-	private int stop;
+	private IMarker start;
+	private IMarker stop;
 	Color color = new Color(null, 200, 120, 255);
 	
-	public MultilineBox (int Start, int Stop) {
+	public MultilineBox (IMarker Start, IMarker Stop) {
 		start = Start;
 		stop = Stop;
 	}
 	
-	public MultilineBox (int Start, int Stop, Color  boxColor) {
+	public MultilineBox (IMarker Start, IMarker Stop, Color  boxColor) {
 		this(Start, Stop);
 		color = boxColor;
 	}
 
 	//getter for start
 	int start() {
-		return start;
+		return AllowEditing.getLineNumber(start);	//TODO: make sure this works with AllowEditing's line number fetching
 	}
 	
 	//getter for stop
 	int stop() {
-		return stop;
+		return AllowEditing.getLineNumber(stop);
+	}
+	
+	public String toString() {
+		String toReturn;
+		toReturn = "Start: " + start + ", Line: " + AllowEditing.getLineNumber(start);
+		toReturn += "\nStop: " + stop + ", Line: " + AllowEditing.getLineNumber(stop);
+		return toReturn;
 	}
 }
